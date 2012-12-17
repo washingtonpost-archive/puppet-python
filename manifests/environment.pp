c77c57f2c15588e36833a13c8d02d69f5ce5bb52
Jason Bartz <jrbartz@gmail.com>
2012-12-17 14:23:44 -0500

    added config for flask

--- manifests/environment.pp
+++ manifests/environment.pp
@@ -1,33 +1,26 @@
 define python::environment(
-    $env_name,
-    $requirements,
-    $upgrade=true,
+    $source,
     $pythonpath=[],
 ) {
-    if $upgrade == true {
-        $pip_upgrade = '-U'
-    } else {
-        $pip_upgrade = ''
-    }
-
     # Create the virtualenv
     exec {"venv_init_${name}":
-        command => "${python::params::init_venv_script} ${python::params::location}${env_name}",
+        command => "${python::params::init_venv_script} ${python::params::location}${name}",
         path => $path,
         user => "${python::params::user}",
         logoutput => true,
+        creates => "${python::params::location}${name}/bin",
     }
 
     file {"add_to_path_${name}":
         ensure => present,
-        path => "${python::params::location}${env_name}/lib/python2.7/site-packages/extra.pth",
+        path => "${python::params::location}${name}/lib/python2.7/site-packages/extra.pth",
         content => template('python/pythonpath.erb'),
         require => Exec["venv_init_${name}"],
     }
 
     # Install all of the python requirements
     exec {"install_${name}":
-        command => "${python::params::location}${env_name}/bin/pip -v --log /tmp/pip.log install ${pip_upgrade} --use-mirrors -r ${requirements}",
+        command => "${python::params::location}${name}/bin/pip -v --log /tmp/pip.${name}.log install -e git+${source}#egg=${name}",
         path => $path,
         logoutput => true,
         user => "${python::params::user}",
---
+++ manifests/flask.pp
@@ -0,0 +1,51 @@
+# python::flask
+
+define python::flask(
+    $source,
+    $repo_name,
+    $flask_config,
+    $location,
+    $vhost,
+    $pythonpath=[],
+    $upgrade=false,
+    $callable='app',
+    $code_path="${python::params::location}",
+) {
+    # include the necessary libs
+    include nginx, uwsgi, git
+
+    # Build a uwsgi instance
+    # All of the requirements here are needed for Django
+    # An additional param, pythonpath, should probably be added.
+    uwsgi::instance::basic {$name:
+        params => {
+            'chdir' => "\"${code_path}${repo_name}/\"",
+            'home' => "\"${code_path}${name}\"",
+            'env' => "\"FLASK_ENV=${flask_config}\"",
+            'module' => "${name}",
+            'callable' => $callable
+
+        }
+    }
+
+    # Create an nginx instance
+    nginx::resource::location { $name:
+        vhost => $vhost,
+        proxy => "unix:/tmp/flask.${name}.sock",
+        uwsgi => true,
+        location => "${location}/",
+        uwsgi_params => ["SCRIPT_NAME ${location}"],
+        notify => Class['nginx::service'],
+        require => Uwsgi::Instance::Basic[$name]
+    }
+
+    # Initialize the environment and install requirements
+    python::environment { $name:
+        source => $source
+        pythonpath => $pythonpath,
+        # Include uwsgi (in order to notify the service that the requirements have finished installing)
+        notify => Class['uwsgi::service'],
+        require => Uwsgi::Instance::Basic[$name]
+
+    }
+}
